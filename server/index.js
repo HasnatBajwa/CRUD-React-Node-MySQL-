2832209cfbdc0dc7e47b738004ae78a3cc59b4a3
HasnatBajwa <hasnatjbajwa@gmail.com>
2023-04-01 15:02:08 +0500

    frst commit

---
+++ server/index.js
@@ -0,0 +1,79 @@
+const express = require("express")
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Making Connection to the server
+const app = express()
+
+
+app.listen(8800, () => {
+    console.log("Connected to Server!");
+});
+
+// Making Connection to the Database
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "products"
+});
+
+// verifying Connection to DB
+
+db.connect((err) => {
+    if (err) {
+        console.log(err.message);
+    }
+    else {
+        console.log("Connection to database Succesful");
+    }
+})
+
+// Activiting Client Data Access
+app.use(express.json())
+
+// Checking data on browser 
+
+app.get("/", (req, res) => {
+    res.json("Hi there I am Your Server Running ")
+})
+
+// Fetching Products from the Data Base
+app.get("/products", (req, res) => {
+    const fetchProducts = "select * from pro";
+    db.query(fetchProducts, (err, data) => {
+        if (err) {
+            res.json(err)
+        }
+        else {
+            res.json(data)
+        }
+    })
+})
+
+// Adding Data to the Database
+
+app.post("/products", (req, res) => {
+    const addProducts = "insert into pro (`brand`, `name`, `model_no`, `image`, `original_price`, `disc_price`, `overview`, `sale_count`, `ratings`) values(?)"
+    const values = [
+        req.body.brand,
+        req.body.name,
+        req.body.model_no,
+        req.body.image,
+        req.body.original_price,
+        req.body.disc_price,
+        req.body.overview,
+        req.body.sale_count,
+        req.body.ratings
+    ];
+    db.query(addProducts, [values], (err, data) => {
+        if (err) {
+            return res.json(err.message)
+        }
+        else {
+            return res.json("Data added Successfully")
+        }
+    })
+});
